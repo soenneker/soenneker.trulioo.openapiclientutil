182e69ec91bf425be542e2f49e7482dad28a1b40
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:54 -0500

    xml commenting

--- src/Soenneker.Trulioo.OpenApiClientUtil/Abstract/ITruliooOpenApiClientUtil.cs
+++ src/Soenneker.Trulioo.OpenApiClientUtil/Abstract/ITruliooOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Trulioo.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ITruliooOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<TruliooOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
